d0daaf767131b34afe66101177c75f44ec4f2809
Jør∂¡ <itsbrickpop@gmail.com>
2019-08-21 20:01:21 +0200

    Adapting the recent changes from the documentation on the Entity

--- dart/gateway.pbjson.dart
+++ dart/gateway.pbjson.dart
@@ -5,25 +5,27 @@
 // @dart = 2.3
 // ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
 
-const GatewaysStore$json = const {
-  '1': 'GatewaysStore',
+const GatewayInfoStore$json = const {
+  '1': 'GatewayInfoStore',
   '2': const [
-    const {'1': 'items', '3': 1, '4': 3, '5': 11, '6': '.dvote.Gateway', '10': 'items'},
+    const {'1': 'items', '3': 1, '4': 3, '5': 11, '6': '.dvote.GatewayInfo', '10': 'items'},
   ],
 };
 
-const Gateway$json = const {
-  '1': 'Gateway',
+const GatewayInfo$json = const {
+  '1': 'GatewayInfo',
   '2': const [
     const {'1': 'dvote', '3': 1, '4': 1, '5': 9, '10': 'dvote'},
-    const {'1': 'web3', '3': 2, '4': 1, '5': 9, '10': 'web3'},
-    const {'1': 'publicKey', '3': 3, '4': 1, '5': 9, '10': 'publicKey'},
-    const {'1': 'meta', '3': 100, '4': 3, '5': 11, '6': '.dvote.Gateway.MetaEntry', '10': 'meta'},
+    const {'1': 'supportedApis', '3': 2, '4': 3, '5': 9, '10': 'supportedApis'},
+    const {'1': 'web3', '3': 3, '4': 1, '5': 9, '10': 'web3'},
+    const {'1': 'networkId', '3': 4, '4': 1, '5': 9, '10': 'networkId'},
+    const {'1': 'publicKey', '3': 5, '4': 1, '5': 9, '10': 'publicKey'},
+    const {'1': 'meta', '3': 100, '4': 3, '5': 11, '6': '.dvote.GatewayInfo.MetaEntry', '10': 'meta'},
   ],
-  '3': const [Gateway_MetaEntry$json],
+  '3': const [GatewayInfo_MetaEntry$json],
 };
 
-const Gateway_MetaEntry$json = const {
+const GatewayInfo_MetaEntry$json = const {
   '1': 'MetaEntry',
   '2': const [
     const {'1': 'key', '3': 1, '4': 1, '5': 9, '10': 'key'},
