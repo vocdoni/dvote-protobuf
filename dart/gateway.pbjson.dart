694352e01113cec05e0c3d722dbe513e6be80075
Jordi Moraleda <jordi.moraleda@gmail.com>
2020-08-31 17:22:21 +0200

    GatewayInfo is not needed for storage

--- dart/gateway.pbjson.dart
+++ dart/gateway.pbjson.dart
@@ -50,32 +50,3 @@ const BootNodeGateways_MetaEntry$json = const {
   '7': const {'7': true},
 };
 
-const GatewayInfoStore$json = const {
-  '1': 'GatewayInfoStore',
-  '2': const [
-    const {'1': 'items', '3': 1, '4': 3, '5': 11, '6': '.dvote.GatewayInfo', '10': 'items'},
-  ],
-};
-
-const GatewayInfo$json = const {
-  '1': 'GatewayInfo',
-  '2': const [
-    const {'1': 'dvote', '3': 1, '4': 1, '5': 9, '10': 'dvote'},
-    const {'1': 'supportedApis', '3': 2, '4': 3, '5': 9, '10': 'supportedApis'},
-    const {'1': 'web3', '3': 3, '4': 1, '5': 9, '10': 'web3'},
-    const {'1': 'networkId', '3': 4, '4': 1, '5': 9, '10': 'networkId'},
-    const {'1': 'publicKey', '3': 5, '4': 1, '5': 9, '10': 'publicKey'},
-    const {'1': 'meta', '3': 100, '4': 3, '5': 11, '6': '.dvote.GatewayInfo.MetaEntry', '10': 'meta'},
-  ],
-  '3': const [GatewayInfo_MetaEntry$json],
-};
-
-const GatewayInfo_MetaEntry$json = const {
-  '1': 'MetaEntry',
-  '2': const [
-    const {'1': 'key', '3': 1, '4': 1, '5': 9, '10': 'key'},
-    const {'1': 'value', '3': 2, '4': 1, '5': 9, '10': 'value'},
-  ],
-  '7': const {'7': true},
-};
-
