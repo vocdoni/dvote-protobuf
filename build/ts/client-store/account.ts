574ee9a076f67a826d91574cc7509635b69f4be9
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2022-09-30 19:59:06 +0200

    build

--- build/ts/client-store/account.ts
+++ build/ts/client-store/account.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import * as _m0 from "protobufjs/minimal";
+import _m0 from "protobufjs/minimal";
 import { EntityReference } from "../metadata/entity";
 import { Wallet } from "./wallet";
 
